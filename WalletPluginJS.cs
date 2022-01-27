b90b136fc568e2ed1a4ef0ae944b20d65759b4f8
CipherProxy <admin@cipherproxy.com>
2022-01-27 01:35:35 -0500

    Initial commit

---
+++ WalletPluginJS.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+/// <summary>
+/// Class with a JS Plugin functions for WebGL.
+/// </summary>
+public class PlugPluginJS : MonoBehaviour
+{
+    /// Importing "PlugConnect"
+    [DllImport("__Internal")]
+    public static extern void PlugConnect();
+
+    private void Start() {
+       PlugConnect();
+   }
+}
---
+++ WalletPluginJS.jslib
@@ -0,0 +1,8 @@
+// Creating functions for the Unity
+mergeInto(LibraryManager.library, {
+    
+    PlugConnect: function () {
+      // Show a message as an alert
+      window.ic.plug.requestConnect()
+   }
+   });
